321bd1be37230d87f0258095ef696a8d2ef8e162
Rachel Moser <moser.rachel@gmail.com>
2023-02-19 14:37:05 -0600

    Chapter2, Exercise 1: Debugging the Rack Environment

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,5 +2,7 @@ class QuotesController < Rulers::Controller
   def a_quote
     "There is nothing either good or bad " +
       "but thinking makes it so."
+      # + env will display everything your application gets from Rack (example: env.json)
+      # "\n<pre>\n#{env}\n</pre>"
   end
 end
