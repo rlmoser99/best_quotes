dd9fd850966a0edb454ce90ffedeae1d1135dc77
Rachel Moser <moser.rachel@gmail.com>
2023-02-19 14:12:02 -0600

    Add Quotes controller and #a_quote action

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 
 require "rulers"
 
+# The LOAD_PATH line lets you load files out of “app/controllers” just by requiring their name, as Rails does. 
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
+# And then you require your new controller.
+require "quotes_controller"
+
 module BestQuotes
   class Application < Rulers::Application
   end
